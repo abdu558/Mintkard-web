96b4c18f724134db84b28f0beb38098bbd6eaea0
a <arefabdu55@gmail.com>
2022-12-12 13:55:34 +0000

    updated

--- mintkard/auth.py
+++ mintkard/auth.py
@@ -1,9 +1,10 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template,request
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login',methods=['GET','POST'])
 def login():
+    
     return render_template("home.html")
 
 
