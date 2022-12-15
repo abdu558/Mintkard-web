03ddf3b589be0b7ca8bcb0b2d359bcceeaedb531
abdu558 <61558312+abdu558@users.noreply.github.com>
2022-12-15 10:31:59 +0000

    Added validation for login

--- mintkard/decks.py
+++ mintkard/decks.py
@@ -2,21 +2,20 @@ from flask import Blueprint, render_template
 
 decks = Blueprint('decks', __name__)
 
-@decks.route('/')
-def home():
-    return render_template("home.html")
+@decks.route('/decks')
+def decks():
+    return render_template("decks.html")
 
 
 @decks.route('/browse')
 def browse():
-    return render_template("home.html")
+    return render_template("browse.html")
 
 
 @decks.route('/study')
 def Study():
-    return render_template("home.html")
+    return render_template("study.html")
 
 # @app.route("/user/<int:id>")
 # def user_detail(id):
 #MAKE EACH CARD HAVE A GET REQUEST WITH THE LINK CHANGING OF THE ID OF THGE CARD MAKING IT SHAREABELK???????????????????????!!!!!!!!
-FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF
