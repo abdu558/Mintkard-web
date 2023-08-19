89283532b37683e3d4fca8d4354aaa8737b79fda
abdu558 <61558312+abdu558@users.noreply.github.com>
2023-08-19 11:06:02 +0000

    updated

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from mintkard import create_app
-
+import os 
 app = create_app()
 
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
 if __name__ == '__main__':
-    app.run(debug=True)
-
-
+    app.run(debug=True, port=os.getenv("PORT", default=5000))
